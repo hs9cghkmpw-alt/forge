ecdd8b3effc22a134ccb3b41cbe5bab57fb02f92
hs9cghkmpw-alt <hs9cghkmpw@privaterelay.appleid.com>
2026-08-30 22:50:09 +0900

    chore: expose GA-1 logic package

---
+++ forge_ai/core/logic/__init__.py
@@ -0,0 +1,5 @@
+"""General App Mode logic primitives."""
+
+from .ga1_logic import ForgeLogicSpec, LogicSpecError, validate_expression
+
+__all__ = ["ForgeLogicSpec", "LogicSpecError", "validate_expression"]
